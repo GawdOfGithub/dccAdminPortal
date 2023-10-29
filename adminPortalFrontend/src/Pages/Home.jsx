4e54e475a19a7baf1744eae6468971e609d3d346
Anurag Bhandari <anuragbhandari77736@gmail.com>
2023-10-29 09:29:38 +0530

    Progress saved

--- adminPortalFrontend/src/Pages/Home.jsx
+++ adminPortalFrontend/src/Pages/Home.jsx
@@ -2,7 +2,7 @@
 const Home = () => {
   return (
     <div className="flex felx-col items-center justify-center mt-40 ">
-      <img src="dcc.svg" style={{ height: "200px", width: "200px" }} alt="Description of the image" />
+      {/* <img src="dcc.svg" style={{ height: "200px", width: "200px" }} alt="Description of the image" /> */}
     </div>
   );
 };
